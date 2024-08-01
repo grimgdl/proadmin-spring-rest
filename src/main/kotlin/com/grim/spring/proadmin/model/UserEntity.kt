eced4db2ca1dcc71c30b92749dbae7dc01e1115e
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2024-08-01 12:58:39 -0600

    improved importing

--- src/main/kotlin/com/grim/spring/proadmin/model/UserEntity.kt
+++ src/main/kotlin/com/grim/spring/proadmin/model/UserEntity.kt
@@ -1,12 +1,6 @@
 package com.grim.spring.proadmin.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonIgnoreType
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 
 @Entity
